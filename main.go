d41215fd75702bca536bb605dcae255fd82f40a0
lakinduakash <lakinduakash@gmail.com>
2019-05-15 15:02:01 +0530

    Add new route

--- main.go
+++ main.go
@@ -5,7 +5,16 @@ import (
     "fmt"
     "log"
     "net/http"
+    "encoding/json"
 )
+
+type Article struct {
+    Title string `json:"Title"`
+    Desc string `json:"desc"`
+    Content string `json:"content"`
+}
+
+type Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -14,8 +23,20 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests() {
     http.HandleFunc("/", homePage)
+    http.HandleFunc("/all", returnAllArticles)
     log.Fatal(http.ListenAndServe(":8081", nil))
 }
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+    articles := Articles{
+        Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+    }
+    fmt.Println("Endpoint Hit: returnAllArticles")
+
+    json.NewEncoder(w).Encode(articles)
+}
+
 
 func main() {
     handleRequests()
