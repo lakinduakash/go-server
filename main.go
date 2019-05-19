3ff91af35aab40a7bde4cb9dcfe2400f9b01112d
lakinduakash <lakinduakash@gmail.com>
2019-05-19 15:25:26 +0530

    go fmt

--- main.go
+++ main.go
@@ -2,42 +2,41 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 type Article struct {
-    Title string `json:"Title"`
-    Desc string `json:"desc"`
-    Content string `json:"content"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
 
 type Articles []Article
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage)
-    http.HandleFunc("/all", returnAllArticles)
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/all", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request){
-    articles := Articles{
-        Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-    }
-    fmt.Println("Endpoint Hit: returnAllArticles")
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	fmt.Println("Endpoint Hit: returnAllArticles")
 
-    json.NewEncoder(w).Encode(articles)
+	json.NewEncoder(w).Encode(articles)
 }
-
 
 func main() {
-    handleRequests()
+	handleRequests()
 }
